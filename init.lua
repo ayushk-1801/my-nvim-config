b3e2976cb6d451bb7e15e24d71686bc168e48dfe
ayushk-1801 <ayush23162@iiitd.ac.in>
2024-02-12 22:40:38 +0530

    First commit

---
+++ init.lua
@@ -0,0 +1,5 @@
+-- bootstrap lazy.nvim, LazyVim and your plugins
+-- Github Token (Password)
+-- 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
+-- git remote set-url origin https://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@github.com/ayushk-1801/<repo-name>
+require("config.lazy")
---
+++ lua/plugins/formatting.lua
@@ -0,0 +1,42 @@
+return {
+  "stevearc/conform.nvim",
+  event = { "BufReadPre", "BufNewFile" },
+
+  config = function()
+    local conform = require("conform")
+
+    conform.setup({
+      formatters_by_ft = {
+        javascript = { "prettier" },
+        typescript = { "prettier" },
+        javascriptreact = { "prettier" },
+        typescriptreact = { "prettier" },
+        -- css = { "prettier" },
+        -- html = { "prettier" },
+        -- json = { "prettier" },
+        -- markdown = { "prettier" },
+
+        lua = { "stylua" },
+
+        python = { "autopep8" },
+
+        cpp = { "clang-format" },
+        c = { "clang-format" },
+      },
+
+      format_on_save = {
+        lsp_fallback = true,
+        async = false,
+        timeout_ms = 500,
+      },
+    })
+
+    vim.keymap.set({ "n", "v" }, "<leader>f", function()
+      conform.format({
+        lsp_fallback = true,
+        async = false,
+        timeout_ms = 500,
+      })
+    end)
+  end,
+}
