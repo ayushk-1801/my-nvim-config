8c0cd5f2f6446612cfdad7cde8831b61c1d4b1c4
ayushk-1801 <ayush.kumara1801@gmail.com>
2024-03-27 18:25:23 +0530

    plugins cleaning

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -9,6 +9,11 @@ local function map(mode, lhs, rhs, opts)
   vim.api.nvim_set_keymap(mode, lhs, rhs, options)
 end
 
+map("n", "<C-d>", "<C-d>zz")
+map("n", "<C-u>", "<C-u>zz")
+map("n", "n", "nzzzv")
+map("n", "N", "Nzzzv")
+
 map("n", "<C-r>", ":CompetiTest run<CR>")
 map("n", "<C-t>", ":CompetiTest receive problem<CR>")
 map("n", "<C-m>", ":CompetiTest add_testcase<CR>")
