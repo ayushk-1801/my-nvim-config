4787cbe592cc16ee4c76d19b5114178c2b30a7ff
ayushk-1801 <ayush23162@iiitd.ac.in>
2024-04-14 20:13:50 +0530

    Changed bufferline configuration to show buffers in tabs

--- lua/ayush/plugins/bufferline.lua
+++ lua/ayush/plugins/bufferline.lua
@@ -1,11 +1,19 @@
 return {
-  "akinsho/bufferline.nvim",
-  dependencies = { "nvim-tree/nvim-web-devicons" },
-  version = "*",
-  opts = {
-    options = {
-      mode = "tabs",
-      separator_style = "slant",
-    },
-  },
+	"akinsho/bufferline.nvim",
+	dependencies = { "nvim-tree/nvim-web-devicons" },
+	version = "*",
+	opts = {
+		options = {
+			mode = "buffers",
+			separator_style = "thin",
+			offsets = {
+				{
+					filetype = "NvimTree",
+					text = "File Explorer",
+					highlight = "Directory",
+					separator = true, -- use a "true" to enable the default, or set your own character
+				},
+			},
+		},
+	},
 }
