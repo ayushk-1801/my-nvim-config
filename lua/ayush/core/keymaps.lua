2c612f3bd27eb44c0d6e835d763bc40d1cabda3a
ayushk-1801 <ayush23162@iiitd.ac.in>
2024-04-22 22:23:24 +0530

    added few keybindings and leetcode.nvim

--- lua/ayush/core/keymaps.lua
+++ lua/ayush/core/keymaps.lua
@@ -6,9 +6,26 @@ local keymap = vim.keymap -- for conciseness
 ---------------------
 -- General Keymaps -------------------
 
+-- move the selected code up or down
+keymap.set("v", "J", ":m '>+1<CR>gv=gv") -- downward
+keymap.set("v", "K", ":m '<-2<CR>gv=gv") -- upward
+
+-- place cursor at the same place when a text is pasted
+keymap.set("n", "J", "mzJ`z")
+
 -- center cursor after scrolling
-keymap.set("n", "<C-u>", "<C-u>zz", { desc = "Center cursor after moving up half-page" })
-keymap.set("n", "<C-d>", "<C-d>zz", { desc = "Center cursor after moving down half-page" })
+keymap.set("n", "<C-u>", "<C-u>zz") -- upward
+keymap.set("n", "<C-d>", "<C-d>zz") -- downward
+
+-- center cursor after finding next/previous occurence
+keymap.set("n", "n", "nzzzv") -- next
+keymap.set("n", "N", "Nzzzv") -- previous
+
+-- greatest remap ever
+keymap.set("x", "<leader>p", '"_dP')
+
+-- make the current file executable
+keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { desc = "Make the current file executable" })
 
 -- use jk to exit insert mode
 keymap.set("i", "jk", "<ESC>", { desc = "Exit insert mode with jk" })
@@ -17,7 +34,7 @@ keymap.set("i", "jk", "<ESC>", { desc = "Exit insert mode with jk" })
 keymap.set("n", "<leader>nh", ":nohl<CR>", { desc = "Clear search highlights" })
 
 -- delete single character without copying into register
--- keymap.set("n", "x", '"_x')
+keymap.set("n", "x", '"_x')
 
 -- increment/decrement numbers
 keymap.set("n", "<leader>+", "<C-a>", { desc = "Increment number" }) -- increment
@@ -34,3 +51,7 @@ keymap.set("n", "<leader>tx", "<cmd>tabclose<CR>", { desc = "Close current tab" 
 keymap.set("n", "<leader>tn", "<cmd>tabn<CR>", { desc = "Go to next tab" }) --  go to next tab
 keymap.set("n", "<leader>tp", "<cmd>tabp<CR>", { desc = "Go to previous tab" }) --  go to previous tab
 keymap.set("n", "<leader>tf", "<cmd>tabnew %<CR>", { desc = "Open current buffer in new tab" }) --  move current buffer to new tab
+
+---------------------
+-- Plugins Keymaps -------------------
+keymap.set("n", "<C-r>", ":RunCode<CR>", { noremap = true, silent = false }) -- code-runner
