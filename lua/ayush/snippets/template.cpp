d9228b42810fe085e79113933977e0b40d9ee146
ayushk-1801 <ayush23162@iiitd.ac.in>
2024-06-24 22:14:20 +0530

    Updated CP template

--- lua/ayush/snippets/template.cpp
+++ lua/ayush/snippets/template.cpp
@@ -55,6 +55,11 @@ template <class T> void _print(multiset <T> v) {cerr << "[ "; for (T i : v) {_pr
 template <class T, class V> void _print(map <T, V> v) {cerr << "[ "; for (auto i : v) {_print(i); cerr << " ";} cerr << "]";}
 void _print(pbds v) {cerr << "[ "; for (auto i : v) {_print(i); cerr << " ";} cerr << "]";}
 
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+
+struct custom_hash {static uint64_t splitmix64(uint64_t x) {x += 0x9e3779b97f4a7c15; x = (x ^ (x >> 30)) * 0xbf58476d1ce4e5b9; x = (x ^ (x >> 27)) * 0x94d049bb133111eb; return x ^ (x >> 31);} size_t operator()(uint64_t x) const { static const uint64_t FIXED_RANDOM = chrono::steady_clock::now().time_since_epoch().count(); return splitmix64(x + FIXED_RANDOM);}};
+/*--------------------------------------------------------------------------------------------------------------------------*/
+ll getRandomNumber(ll l, ll r) {return uniform_int_distribution<ll>(l, r)(rng);} 
 /*--------------------------------------------------------------------------------------------------------------------------*/
 
 void solve() {
