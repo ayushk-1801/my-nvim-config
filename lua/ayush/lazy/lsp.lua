96bf393cc1fe94dc46fb4194bafbc39b7ebc829a
ayushk-1801 <ayush23162@iiitd.ac.in>
2024-07-29 19:46:07 +0530

    Updated lsp

--- lua/ayush/lazy/lsp.lua
+++ lua/ayush/lazy/lsp.lua
@@ -95,7 +95,17 @@ return {
                     { name = 'buffer' },
                 }),
             formatting = {
+                expandable_indicator = false,
+                fields = { "abbr", "kind", "menu" },
                 format = require("lspkind").cmp_format {
+                    mode = "symbol_text",
+                    -- maxwidth = 50, -- prevent the popup from showing more than provided characters (e.g 50 will not show more than 50 characters)
+                    -- can also be a function to dynamically calculate max width such as
+                    maxwidth = function()
+                        return math.floor(0.45 * vim.o.columns)
+                    end,
+                    ellipsis_char = "...", -- when popup menu exceed maxwidth, the truncated part would show ellipsis_char instead (must define maxwidth first)
+                    show_labelDetails = false, -- show labelDetails in menu. Disabled by default
                     menu = {
                         nvim_lsp = "[LSP]",
                         path = "[PATH]",
