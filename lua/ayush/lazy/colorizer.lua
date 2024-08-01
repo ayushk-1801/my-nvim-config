f2d2d19e35935c7dc14dc3acc426b34ee136573d
ayushk-1801 <ayush23162@iiitd.ac.in>
2024-08-01 22:44:59 +0530

    Automatically run colorizer when neovim starts

--- lua/ayush/lazy/colorizer.lua
+++ lua/ayush/lazy/colorizer.lua
@@ -1,4 +1,12 @@
 return {
-	"norcalli/nvim-colorizer.lua",
-	opts = {},
+    "norcalli/nvim-colorizer.lua",
+    opts = {},
+    config = function()
+        require('colorizer').setup()
+        vim.api.nvim_create_autocmd("VimEnter", {
+            pattern = "*",
+            command = "ColorizerAttachToBuffer"
+        })
+    end
 }
+
